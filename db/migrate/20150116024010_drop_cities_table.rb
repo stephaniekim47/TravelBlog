45daf914ddc1a2854600e5252e6566f6dc94306c
Stephanie Kim <stephanie.kim@berkeley.edu>
2015-01-21 10:55:00 -0800

    migrate

--- db/migrate/20150116024010_drop_cities_table.rb
+++
@@ -1,9 +0,0 @@
-class DropCitiesTable < ActiveRecord::Migration
-  def up
-  	drop_table :cities
-  end
-
-  def down
-  	raise ActiveRecord::IrreversibleMigration
-  end
-end
--- db/migrate/20150116045628_drop_cities_table2.rb
+++
@@ -1,9 +0,0 @@
-class DropCitiesTable2 < ActiveRecord::Migration
-  def up
-  	drop_table :cities
-  end
-
-  def down
-  	raise ActiveRecord::IrreversibleMigration
-  end
-end
