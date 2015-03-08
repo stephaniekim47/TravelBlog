de99622050d890a93ce14a45f4a0f6d825d51301
Stephanie Kim <stephanie.kim@berkeley.edu>
2015-03-08 01:26:59 -0800

    s3

--- app/models/country.rb
+++ app/models/country.rb
@@ -2,7 +2,9 @@ class Country < ActiveRecord::Base
       has_many :cities, dependent: :destroy
       belongs_to :user
 
-      has_attached_file :photo, :styles => { :small => "300x300#", :medium => "1000x600#" }
+      has_attached_file :photo, :styles => { :small => "300x300#", :medium => "1000x600#" }, :storage => :s3,
+            :bucket  => ENV['travel123']
+
 
       validates :photo, presence: true, :attachment_content_type => { :content_type => ['image/png', 'image/jpg', 'image/jpeg']}
       validates :title, presence: true, uniqueness: true
---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,2 @@
+Paperclip::Attachment.default_options[:url] = 'travel123'
+Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
