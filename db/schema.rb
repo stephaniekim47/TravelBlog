6a295999a20b14765776b2e5a77bc2dd45f022df
Stephanie Kim <stephanie.kim@berkeley.edu>
2015-03-08 01:51:57 -0800

    s3

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150121052735) do
+ActiveRecord::Schema.define(version: 20150308094130) do
 
   create_table "cities", force: :cascade do |t|
     t.string   "title"
