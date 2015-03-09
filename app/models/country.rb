68c697cbc61b76bebc8dcd734e98fbd275f04e71
Stephanie Kim <stephanie.kim@berkeley.edu>
2015-03-09 01:30:58 -0700

    s3

--- app/models/country.rb
+++ app/models/country.rb
@@ -2,9 +2,18 @@ class Country < ActiveRecord::Base
       has_many :cities, dependent: :destroy
       belongs_to :user
 
-      has_attached_file :photo, :styles => { :small => "300x300#", :medium => "1000x600#" }, :storage => :s3,
-            :bucket  => ENV['S3_BUCKET_NAME']
+      paperclip_opts = {
+      	:styles => { :small => "300x300#", :medium => "1000x600#" }
+      }
+      if Rails.env.development?
+      	paperclip_opts.merge! :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
+    						  :url => "/system/:attachment/:id/:style/:filename"
+      else
+      	paperclip_opts.merge! :storage => :s3,
+      						  :bucket  => ENV['S3_BUCKET_NAME']
+      end 
 
+      has_attached_file :photo, paperclip_opts
 
       validates :photo, presence: true, :attachment_content_type => { :content_type => ['image/png', 'image/jpg', 'image/jpeg']}
       validates :title, presence: true, uniqueness: true
