9cfc07989f13883daa040f7cb69a873f9df2cafe
Stephanie Kim <stephanie.kim@berkeley.edu>
2015-03-11 23:22:59 -0700

    migrations

--- db/migrate/20150311093836_add_attributes_to_profiles.rb
+++
@@ -1,6 +0,0 @@
-class AddAttributesToProfiles < ActiveRecord::Migration
-  def change
-    add_column :profiles, :name, :string
-    add_column :profiles, :photo, :attachment
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150312042959) do
+ActiveRecord::Schema.define(version: 20150312060024) do
 
   create_table "cities", force: :cascade do |t|
     t.string   "title"
@@ -42,10 +42,37 @@ ActiveRecord::Schema.define(version: 20150312042959) do
 
   add_index "countries", ["user_id"], name: "index_countries_on_user_id"
 
-# Could not dump table "profiles" because of following NoMethodError
-#   undefined method `[]' for nil:NilClass
+  create_table "profiles", force: :cascade do |t|
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
+    t.integer  "user_id"
+    t.string   "name"
+  end
 
-# Could not dump table "users" because of following NoMethodError
-#   undefined method `[]' for nil:NilClass
+  add_index "profiles", ["user_id"], name: "index_profiles_on_user_id"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "role"
+    t.boolean  "admin"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
