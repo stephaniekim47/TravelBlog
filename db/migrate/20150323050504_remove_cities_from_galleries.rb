0fecd3940198a69d21b9eaa3e1762a4ecded9e11
Stephanie Kim <stephanie.kim@berkeley.edu>
2015-03-24 23:29:08 -0700

    migrations

--- db/migrate/20150323050504_remove_cities_from_galleries.rb
+++
@@ -1,5 +0,0 @@
-class RemoveCitiesFromGalleries < ActiveRecord::Migration
-  def change
-    remove_column :galleries, :cities_id, :integer
-  end
-end
--- db/migrate/20150323055852_remove_photo_from_galleries.rb
+++
@@ -1,9 +0,0 @@
-class RemovePhotoFromGalleries < ActiveRecord::Migration
-  def change
-  	remove_column :galleries, :city_id, :integer
-  	remove_column :galleries, :photo_file_name, :string
-  	remove_column :galleries, :photo_content_type, :string
-  	remove_column :galleries, :photo_file_size, :integer
-  	remove_column :galleries, :photo_updated_at, :datetime
-  end
-end
